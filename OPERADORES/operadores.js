de34b1682585c8097db65cda7bdbadbda057c8b8
Gal <134517591+GabrielLuizdaS@users.noreply.github.com>
2023-08-22 08:19:01 -0300

    Add files via upload

---
+++ OPERADORES/operadores.js
@@ -0,0 +1,24 @@
+function somar(){
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+    var result = parseInt(valor1) + parseInt(valor2)
+    alert(result);
+}
+function sub(){
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+    var result = parseInt(valor1) - parseInt(valor2)
+    alert(result);
+}
+function div(){
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+    var result = parseInt(valor1) / parseInt(valor2)
+    alert(result);
+}
+function mult(){
+    var valor1 = document.getElementById('valor1').value;
+    var valor2 = document.getElementById('valor2').value;
+    var result = parseInt(valor1) * parseInt(valor2)
+    alert(result);
+}
